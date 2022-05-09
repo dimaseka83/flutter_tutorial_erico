c5531d2f44fb8a7226e85295d17941a5f2b8a447
dhimaseka <dimaseka83@gmail.com>
2022-05-09 13:44:19 +0700

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,36 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0; // inisialisasi variabel integer number
+  void tekanTombol(){ // method yang tidak mengembalikan apa apa
+    setState(() { // digunakan merefresh tampilan di halaman
+      number++; // perintah untuk menambah variabel integer
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Latihan Container")),
-        body: Container(
-            color: Colors.red, // membuat warna didalam container
-            // margin: EdgeInsets.all(10), // membuat margin didalam container dengan seluruh margin ukuran 10
-            margin: EdgeInsets.fromLTRB(10, 15, 20,
-                25), // membuat margin dengan ukuran segala sisi yang bisa dicustom
-            // padding: EdgeInsets.all(10), // membuat padding didalam container dengan seluruh padding ukuran 10
-            padding: EdgeInsets.only(
-                bottom: 20,
-                top:
-                    20), // membuat padding didalam container dengan ukuran satu sisi
-            child: Container(
-              // color: Colors.blue,
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(  //menambahkan styling pada container
-                  borderRadius: BorderRadius.circular(20), // membuat rounded pada container
-                  gradient: LinearGradient( // membuat gradasi warna
-                      begin: Alignment.topLeft, // membuat arah gradasi warna awal
-                      end: Alignment.bottomRight, // membuat arah gradasi warna akhir
-                      colors: <Color>[Colors.amber, Colors.blue])), //menambahkan dua warna yang digradasi
-            )),
+        appBar: AppBar(title: Text("Stateful Widget Demo"),),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString(), style: TextStyle(fontSize: 10 + number.toDouble()),),
+              RaisedButton(
+                child: Text("Tambah Bilangan"),
+                onPressed: tekanTombol,  // mengarah ke method tekanTombol
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
