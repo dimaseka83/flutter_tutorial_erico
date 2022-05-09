4e2fa91b7b8379144fae0b6d80aa6ae47410af64
dhimaseka <dimaseka83@gmail.com>
2022-05-09 10:45:05 +0700

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Aplikasi Hello World"),),
-        body: Center(child: Text("Hello World")),
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 100,
+                child: Text(
+                  "Saya sedang melatih kemampuan flutter saya dan saya ingin berkembang",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20), // membuat styling text
+                  // textAlign: TextAlign.center, // membuat text ke tengah
+                  // maxLines: 2,  // membuat text hanya 2 baris
+                  // overflow: TextOverflow.clip, // membuat text jika lebih dari maxlines maka akan dipotong
+                  // softWrap: false, // membuat text menjadi rata satu baris
+                ))),
       ),
     );
   }
