f09b1d8fb9c45408b7e2c982cd7557a08c66dbcd
dhimaseka <dimaseka83@gmail.com>
2022-05-10 16:02:37 +0700

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(      
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Image"),
+          title: Text("Spacer Widget"),
         ),
         body: Center(
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 200,
-            padding: EdgeInsets.all(3),
-            // Menambahkan gambar di dalam container
-            child: Image(
-              // sumber gambar dari link
-              // image: NetworkImage(
-              //     "https://ecs7.tokopedia.net/blog-tokopedia-com/uploads/2021/01/scmc.jpg"),
-              //     fit: BoxFit.contain,
-              //     repeat: ImageRepeat.repeat,
-
-              // sumber gambar dari internal aplikasi
-              image: AssetImage(
-                  "images/lisa.png"),
-                  fit: BoxFit.contain,
-                  repeat: ImageRepeat.repeat,
-            ),
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Spacer(flex: 1,),
+              Container(width: 80, height: 80, color: Colors.red,),
+              Spacer(flex: 2,),
+              Container(width: 80, height: 80, color: Colors.green,),
+              Spacer(flex: 3,),
+              Container(width: 80, height: 80, color: Colors.blue,),
+              Spacer(flex: 2,)
+            ],
           ),
         ),
       ),
