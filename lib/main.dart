b8608b8aa2aab4fa4326d38f1e1d38c5058c320b
dhimaseka <dimaseka83@gmail.com>
2022-05-10 12:11:03 +0700

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,63 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random(); // objek random
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Animated Container"),
+        appBar: AppBar(title: Text("Flexible Layout")),
+        body: Column(
+          children: <Widget>[
+            Flexible( // Widget untuk membuat container responsive
+              flex: 1, // perbandingan untuk membagi layar
+              child: Row( // Membuat Container didalam row
+                children: <Widget>[
+                  Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.red,
+                )
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                )
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.purple,
+                )
+              ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.amber,
+              )
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.blue,
+              )
+            ),
+          ],
         ),
-        body: Center(
-            child: GestureDetector( // untuk mendeteksi sebuah aksi trigger dari user 
-          onTap: () { // suatu method aksi
-            setState(() {}); // jika onTap ada trigger maka method ini akan jalan
-          },
-          child: AnimatedContainer( // animasi
-            color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), // membuat warna model argb(a = transparent, r = red, g = green, b = blue) secara random jika ada trigger method setState
-                random.nextInt(256)),
-            duration: Duration(seconds: 1), // durasi animasi
-            width: 50.0 + random.nextInt(101), // membuat lebar secara random jika ada trigger method setState
-            height: 50.0 + random.nextInt(101), // membuat tinggi secara random jika ada trigger method setState
-          ),
-        )),
       ),
     );
   }
 }
+
+// catatan
+// jika didalam column diimplementasikan flexible, maka seluruh row atau container harus dibungkus widget flexible, jika tidak diimplementasikan maka row atau container tidak akan muncul di layar
